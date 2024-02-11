962455e1786cd1b332720683d2d22d9494f9e588
Elena <barsukovae93@gmail.com>
2024-02-11 21:17:00 +0300

    для запуска

--- advertisements/advertisements/settings.py
+++ advertisements/advertisements/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'advertisements.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [Path.joinpath(BASE_DIR, 'template')],                 # указываем ссылку на папку с html-шаблонами
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -118,6 +118,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [
+    Path.joinpath(BASE_DIR, 'static') # joinpath - объединение путей до файла
+]                                     # BASE_DIR - корневая директория
+                                      # 'static' - папка со статическими файлами
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
--- advertisements/app_advertisement/views.py
+++ advertisements/app_advertisement/views.py
@@ -1,6 +1,6 @@
 from django.http import HttpResponse        # импорт объекта response для ответа сервера клиенту
 from django.shortcuts import render
 
-def index(request):                         # функция, возвращающая ответ "Успешно" на запрос пользователя
-    return HttpResponse('Успешно')
-
+def index(request):                         
+    # return HttpResponse('Успешно')        # функция, возвращающая ответ "Успешно" на запрос пользователя
+    return render(request, 'index.html')    # подключение к основному коду html-шаблона, который будет определять внешний вид главной страницы сайта 
