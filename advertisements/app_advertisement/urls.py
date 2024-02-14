412acd5cc016c5a6472491b371d6de55a7bb58f7
Elena <barsukovae93@gmail.com>
2024-02-14 21:56:04 +0300

    Домашка модуль 4 занятие 1

--- advertisements/app_advertisement/urls.py
+++ advertisements/app_advertisement/urls.py
@@ -1,6 +1,7 @@
-from django.urls import path      # импорт объекта для связи папки и url-адреса 
-from .views import index          # импортируем функцию index
+from django.urls import path                   # импорт объекта для связи папки и url-адреса 
+from .views import index, top_sellers          # импортируем функцию index
 
-urlpatterns = [                   # связка функции с ответом сервера с сайтом
-    path('',index)
+urlpatterns = [                                # связка функции с ответом сервера с сайтом
+    path('',index, name='main_page'),
+    path('top_sellers/', top_sellers, name='top_sellers')
 ]
--- advertisements/app_advertisement/views.py
+++ advertisements/app_advertisement/views.py
@@ -4,3 +4,7 @@ from django.shortcuts import render
 def index(request):                         
     # return HttpResponse('Успешно')        # функция, возвращающая ответ "Успешно" на запрос пользователя
     return render(request, 'index.html')    # подключение к основному коду html-шаблона, который будет определять внешний вид главной страницы сайта 
+
+def top_sellers(request):                      # функция render отправляет что-либо на сервер(сайт) 
+    return render(request, 'top-sellers.html') # в кавычках прописывается название файла, который необходимо отправить на сервер(сайт)
+
